3ca18ac233732845b2958f54fd4fcf5e7a94aca3
Johnny Leandro Valenzuela Phocco <johnnyvalenzuelap@gmail.com>
2019-02-10 22:02:10 -0500

    identacion

--- TOP_format_files.py
+++ TOP_format_files.py
@@ -9,22 +9,14 @@ def transform_files():
 
     for key in FILE_DICT:
         
-        LOCAL_KEY = '{path}{filename}'.format(path=LOCAL_PATH_DOWNLOAD,
-        										filename=key)
+        LOCAL_KEY = '{path}{filename}'.format(path=LOCAL_PATH_DOWNLOAD, filename=key)
         
-        df = pd.read_csv(LOCAL_KEY, sep=',', 
-        							encoding='utf-8',
-        							names=FILE_DICT[key]['values'], 
-        							dtype=str)
+        df = pd.read_csv(LOCAL_KEY, sep=',', encoding='utf-8',names=FILE_DICT[key]['values'], dtype=str)
 
         if key != CSV_FILE:
             
-            df.to_excel('{path}{excelfile}'.format(path=LOCAL_PATH_FORMAT, 
-            										excelfile=FILE_DICT[key]['file_name']),
-            								index = False)
+            df.to_excel('{path}{excelfile}'.format(path=LOCAL_PATH_FORMAT, excelfile=FILE_DICT[key]['file_name']),index = False)
         
         else:
-            df.to_csv('{path}{csvfilename}'.format(path=LOCAL_PATH_FORMAT, 
-            										csvfilename=FILE_DICT[key]['file_name']), 
-            								index= False)
+            df.to_csv('{path}{csvfilename}'.format(path=LOCAL_PATH_FORMAT,csvfilename=FILE_DICT[key]['file_name']), index= False)
 
